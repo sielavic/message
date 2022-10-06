e063874cb2101ce526f5faab3084c10f3c4ad358
sielavic <63396872+sielavic@users.noreply.github.com>
2022-10-06 15:59:47 +0300

    Create message.php

---
+++ message.php
@@ -0,0 +1,15 @@
+ <? if(!isset($timeLine['content']['polls']['answers_result'])){?>
+                            <? if(!empty($timeLine['content']['discription'])) {
+                                $name = mb_strimwidth($timeLine['content']['discription'], 0, 10, "...");
+                                $timeLineRoom =  Entity\Room::where('work_id', $timeLine['id'])->get();
+                                if (isset($timeLineRoom[0])){
+                                    if ( !in_array($timeLine['id'], (array)$timeLineRoom[0]) ){
+                                        $room = Entity\Room::create(['name' => $name, 'funder' => USER_COOKIE_ID, 	'work_id' => $timeLine['id'], 'updated_at' => date('Y-m-d H:i:s')]);
+                                        Entity\RoomUser::create(['room_id' => $room->id, 'user_id' => USER_COOKIE_ID]);
+                                        $my_dialog = $room;
+                                    }else{
+                                        $room = $timeLineRoom[0];
+                                        $my_dialog = $room;
+                                    }
+                                }
+                            }?>
